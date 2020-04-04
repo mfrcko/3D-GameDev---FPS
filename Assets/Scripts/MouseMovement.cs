4f60ea18b062103404901309eb317167d1afd128
mfrcko <37274445+mfrcko@users.noreply.github.com>
2020-04-04 23:29:41 +0200

    Added mouse movement and basic player controller

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    //Public
+    public float mouseSensitivity = 100f;
+    public Transform playerTransform;
+
+    //Private
+    float xRotate = 0f;
+    
+    void Start()
+    {
+        //Cursor always in the middle of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+   
+    void Update()
+    {
+        //Data about mouse movement inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotate -= mouseY;
+        xRotate = Mathf.Clamp(xRotate, -90, 90f);
+
+        //Apllying rotation
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f); 
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
