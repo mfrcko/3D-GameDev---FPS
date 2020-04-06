8157c16f5ccb2943c6a1ee460f88359c005c3e62
mfrcko <37274445+mfrcko@users.noreply.github.com>
2020-04-07 00:07:43 +0200

    Gun/Shooting mechanic added
    Added a gun with hit and muzzle particles that register hit on objects via Raycast.

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    //Public
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzle;
+    public GameObject impactEffect;
+
+    //Private
+    private float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        //Shoots with the desired fire rate
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzle.Play(); //Plays muzzle particle
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name); //Displays what object was hit
+
+            GameObject impactGameObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal)); //Instantiates impact particle
+            Destroy(impactGameObject, 6f); //Destroys impact particle after time
+        }
+
+    }
+}
